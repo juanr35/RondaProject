8f6360faa5b2fe91cb6b4657e49b47e6a0e5fdcd
DavLink <davega066@gmail.com>
2017-04-07 20:54:00 -0400

    Menu
    Menu y musica de menu inicial.

---
+++ Ronda Project/Assets/ControlAudio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlAudio : MonoBehaviour {
+
+	public AudioClip musica;
+
+	private AudioSource player;
+
+	void Awake(){
+
+		player = GetComponent<AudioSource>();
+
+		if(FindObjectsOfType(GetType()).Length == 1){
+			player.clip = musica;
+			player.Play();
+		}
+
+	}
+
+	void Update(){
+
+		DontDestroyOnLoad(player);
+
+	}
+}
---
+++ Ronda Project/Assets/Controlador.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controlador : MonoBehaviour {
+
+	public string escena;
+
+	public void _changeScene(){
+
+		SceneManager.LoadScene(escena);
+	}
+}
