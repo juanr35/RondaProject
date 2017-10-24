55a58c07c8882f6de04df0679d39c5dfcf47e769
DavLink <davega066@gmail.com>
2017-10-24 12:03:51 -0400

    1.0
    Juego Base en funcionamiento, funcion de las fichas, diferencia de cada ficha

---
+++ RondaProyect1.0/Assets/CelulaDefault/ControllerCelulaDefault.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCelulaDefault : MonoBehaviour {
+
+	bool touch;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () 
+	{
+		touch = false;	
+		animator = GetComponent<Animator>();
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if( touch) 
+			animator.SetTrigger("Activar");
+	}
+
+	void DeleteCelula()
+	{
+		Destroy(gameObject);
+	}
+
+	public void EnaTouch()
+	{
+		touch = true;
+	}		
+}
---
+++ RondaProyect1.0/Assets/CelulaDividir/ControllerCelulaDividir.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCelulaDividir : MonoBehaviour {
+
+	bool touch;
+	Animator animator;
+	public GameObject celulaPrefab;
+	public int setX;
+
+	// Use this for initialization
+	void Start () 
+	{
+		touch = false;	
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if( touch) {
+			animator.SetTrigger("Activar");
+			touch = false;
+		}		
+	}
+
+	public void EnaTouch()
+	{
+		touch = true;
+	}
+
+	public void Divide()
+	{
+		GameObject celula1 = Instantiate( celulaPrefab, transform.position, Quaternion.identity);
+		celula1.transform.parent = transform;
+		celula1.transform.localPosition = new Vector3( 1 * setX, 0, 0);
+		celula1.transform.parent = transform.parent;
+
+		GameObject celula2 = Instantiate( celulaPrefab, transform.position, Quaternion.identity);
+		celula2.transform.parent = transform;
+		celula2.transform.localPosition = new Vector3( -1 * setX, 0, 0);
+		celula2.transform.parent = transform.parent;
+
+		Destroy( gameObject);
+	}
+		
+}
