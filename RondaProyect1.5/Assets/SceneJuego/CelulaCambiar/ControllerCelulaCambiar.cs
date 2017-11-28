f192c04d8f72b8bfee6f44e064cb0ee259379641
Juan R <juanr35@gmail.com>
2017-11-27 22:25:42 -0400

    Version 1.5
    En esta version agrega una scena llamada Menu correspondiente al menu principal del juego con opciones como jugar y salir, tambien en la scena juego ya cumple con total funcionalidad los tres botones correspondientes al turno y pausa del juego donde se enlazan las dos scenas.

---
+++ RondaProyect1.5/Assets/SceneJuego/CelulaCambiar/ControllerCelulaCambiar.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCelulaCambiar : MonoBehaviour {
+
+	static bool delete;
+	static int numCeCam;
+	static int ceCamDel;
+	bool touch;
+	Animator animator;
+	public GameObject celulaRed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		touch = false;	
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if( touch) {
+			animator.SetTrigger("Activar");
+			touch = false;
+		}
+
+		if( delete) {
+			Destroy( gameObject);
+			numCeCam--;
+			_ReiniciarCeCamDel();
+		}
+	}
+
+	public void _EnaTouch()
+	{
+		touch = true;
+	}
+
+	public void _Cambiar()
+	{
+		GameObject ceRed = Instantiate( celulaRed, transform.position, Quaternion.identity);
+		ceRed.transform.SetParent( transform.parent, false);
+		ceRed.transform.SetPositionAndRotation( transform.position, Quaternion.identity);
+
+		numCeCam--;
+		ceCamDel++;
+		Destroy( gameObject);
+	}
+
+	public void _Delete()
+	{
+		delete = true;
+	}
+
+	public void _NotDelete()
+	{
+		delete = false;
+	}
+
+	public void _NumCeCamAum()
+	{
+		numCeCam++;
+	}
+
+	public int _NumCeCam()
+	{
+		return numCeCam;
+	}
+
+	public void _ReiniciarCeCamDel()
+	{
+		ceCamDel = 0;
+	}
+
+	public int _NumCeCamDel()
+	{
+		return ceCamDel;
+	}
+}
---
+++ RondaProyect1.5/Assets/SceneJuego/ControlPause.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ControlPause : MonoBehaviour {
+
+	public Canvas canvas;
+	public Canvas canvasPause;
+
+	// Use this for initialization
+	void Start () 
+	{
+		canvasPause.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void _Pause()
+	{
+		Time.timeScale = 0;
+		canvas.enabled = false;
+		canvasPause.enabled = true;
+	}
+
+	public void _Continuar()
+	{
+		Time.timeScale = 1;
+		canvas.enabled = true;
+		canvasPause.enabled = false;
+	}
+
+	public void _Salir()
+	{
+		Time.timeScale = 1;
+		SceneManager.LoadScene( "Menu");
+	}
+}
